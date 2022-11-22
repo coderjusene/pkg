4d5f2e3a59bca4e3856b6e9209832005147f6bb0
jusene <13588722443@163.com>
2022-11-22 17:05:43 +0800

    nacos

---
+++ utils/nacos_client.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"flowctl/models"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/spf13/cobra"
+	"io"
+	"text/template"
+)
+
+type NacosClient struct {
+	client config_client.IConfigClient
+}
+
+func NewNacosClient(addr, namespace string, port uint64) *NacosClient {
+	serviceConfig := []constant.ServerConfig{
+		{
+			IpAddr: addr,
+			Port:   port,
+		},
+	}
+
+	// 创建clientConfig
+	clientConfig := constant.ClientConfig{
+		TimeoutMs:           5000,
+		NamespaceId:         namespace,
+		CacheDir:            "/tmp/nacos/cache",
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/logs",
+		LogLevel:            "debug",
+		AppendToStdout:      false,
+	}
+
+	// 创建动态配置客户端
+	configClient, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serviceConfig,
+		},
+	)
+
+	cobra.CheckErr(err)
+
+	return &NacosClient{client: configClient}
+}
+
